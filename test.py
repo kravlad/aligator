0c2406e6f6b4a4291ff5cbb5e80f3bf106d82360
kravlad <kravlad86@gmail.com>
2022-07-03 13:45:18 -0700

    update

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
 import os
+import requests
 from datetime import datetime, timedelta
 
-
-# os.environ['token'] = '5056480203:AAFEYxAa1GY18S2AT6StaphieeMm5WxKo3M'
-# region = os.environ
-# print(dict(region))
-# print(region.get('TOKEN'))
-
-x = [0, -1, -2, -3]
-print(x['-1'])
+r = requests.get('https://www.instagram.com/p/CfbLh4fjLzV')
+pass
